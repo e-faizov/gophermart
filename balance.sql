ca3c086256ca1459cfe3b19e641f25a33dc1e392
Eldar Faizov <e.faizov@qiwi.com>
2022-12-04 09:18:47 +0300

    remove

--- balance.sql
+++
@@ -1,8 +0,0 @@
-create table balances
-(
-    user_id int
-        constraint balances_pk
-            primary key,
-    balance float8
-        constraint balances_nonnegative check(balance >= 0)
-)
